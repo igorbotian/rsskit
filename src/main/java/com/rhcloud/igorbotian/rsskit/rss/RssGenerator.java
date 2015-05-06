f41330e9475ff80cca89eee8ff9ce1de12fe8967
Igor Botian <igor.botian@gmail.com>
2015-05-06 20:32:00 +0300

    In case of error, a whole error message chain is printed out

--- src/main/java/com/rhcloud/igorbotian/rsskit/rss/RssGenerator.java
+++ src/main/java/com/rhcloud/igorbotian/rsskit/rss/RssGenerator.java
@@ -1,12 +1,10 @@
 package com.rhcloud.igorbotian.rsskit.rss;
 
 import com.rometools.rome.feed.synd.*;
+import org.apache.commons.lang3.StringUtils;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 /**
  * @author Igor Botian <igor.botian@gmail.com>
@@ -27,23 +25,62 @@ public abstract class RssGenerator<T> {
         rss.setAuthor(skeleton.getAuthor());
         rss.setFeedType(skeleton.getFeedType());
 
+        List<SyndEntry> entries = new ArrayList<>();
+        entries.addAll(skeleton.getEntries());
+        entries.add(generateErrorEntry(ex));
+
+        rss.setEntries(entries);
+
+        return rss;
+    }
+
+    private SyndEntry generateErrorEntry(Exception ex) {
+        assert ex != null;
+
         SyndEntry entry = new SyndEntryImpl();
 
         SyndContent description = new SyndContentImpl();
-        description.setType("text/plain");
-        description.setValue(ex.getMessage());
+        description.setType("text/html");
+        description.setValue(printErrorMessages(ex));
 
         entry.setTitle("Error (" + UTC_FORMAT.format(new Date()) + ")");
         entry.setDescription(description);
         entry.setPublishedDate(new Date());
 
-        List<SyndEntry> entries = new ArrayList<>();
-        entries.addAll(skeleton.getEntries());
-        entries.add(entry);
+        return entry;
+    }
 
-        rss.setEntries(entries);
+    private String printErrorMessages(Exception ex) {
+        assert ex != null;
 
-        return rss;
+        StringBuilder text = new StringBuilder();
+
+        List<String> messages = gatherErrorMessages(ex);
+
+        for(int i = 0; i < messages.size(); i++) {
+            if(i > 0) {
+                text.append("<br/>");
+                text.append(StringUtils.repeat("&nbsp;&nbsp;", i));
+                text.append("&#8627;");
+                text.append("&nbsp;");
+            }
+
+            text.append(messages.get(i));
+        }
+
+        return text.toString();
+    }
+
+    private List<String> gatherErrorMessages(Exception ex) {
+        List<String> messages = new ArrayList<>();
+        Throwable it = ex;
+
+        while(it != null) {
+            messages.add(it.getMessage());
+            it = it.getCause();
+        }
+
+        return messages;
     }
 
     public abstract SyndFeed generate(T obj);
