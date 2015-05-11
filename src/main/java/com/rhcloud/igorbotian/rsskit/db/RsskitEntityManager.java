e2ed2275ffdb0203832f283394c7ee8fce0b8925
Igor Botian <igor.botian@gmail.com>
2015-05-11 18:40:46 +0300

    Java 1.8 Base64 implementation is replaced by one from Apache Commons Codec

--- src/main/java/com/rhcloud/igorbotian/rsskit/db/RsskitEntityManager.java
+++ src/main/java/com/rhcloud/igorbotian/rsskit/db/RsskitEntityManager.java
@@ -1,11 +1,11 @@
 package com.rhcloud.igorbotian.rsskit.db;
 
 import com.j256.ormlite.db.DatabaseType;
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.tomcat.jdbc.pool.DataSource;
 
 import java.sql.SQLException;
-import java.util.Base64;
 import java.util.Objects;
 
 /**
@@ -25,6 +25,6 @@ public abstract class RsskitEntityManager {
 
     protected String generateToken(String accessToken) {
         assert accessToken != null;
-        return Base64.getEncoder().encodeToString(DigestUtils.md5(accessToken));
+        return Base64.encodeBase64String(DigestUtils.md5(accessToken));
     }
 }
