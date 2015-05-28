3d4a3a57b2052485e585b3851bdeb19adfbf3dbe
Igor Botian <igor.botian@gmail.com>
2015-05-28 18:48:55 +0300

    Better representation of championat.com RSS feed entries

--- src/main/java/com/rhcloud/igorbotian/rsskit/rss/championat/ChampionatRssFeedModifier.java
+++ src/main/java/com/rhcloud/igorbotian/rsskit/rss/championat/ChampionatRssFeedModifier.java
@@ -4,12 +4,21 @@ import com.rhcloud.igorbotian.rsskit.mobilizer.Mobilizers;
 import com.rhcloud.igorbotian.rsskit.rss.LinkMapper;
 import com.rhcloud.igorbotian.rsskit.rss.RssModifier;
 import com.rhcloud.igorbotian.rsskit.utils.RSSUtils;
+import com.rometools.rome.feed.synd.SyndContent;
+import com.rometools.rome.feed.synd.SyndContentImpl;
+import com.rometools.rome.feed.synd.SyndEntry;
 import com.rometools.rome.feed.synd.SyndFeed;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -27,7 +36,43 @@ public class ChampionatRssFeedModifier implements RssModifier {
 
         RSSUtils.filterByCategories(feed, Collections.singleton("breaking"));
         RSSUtils.mapLinks(feed, mobileVersionLinkMapper);
-        RSSUtils.extendDescription(feed, Mobilizers.readability());
+        extendDescription(feed);
+    }
+
+    private void extendDescription(SyndFeed feed) {
+        assert feed != null;
+
+        for(SyndEntry entry : feed.getEntries()) {
+            String description;
+
+            try {
+                description = getExtendedDescription(entry);
+            } catch (IOException e) {
+                try {
+                    description = Mobilizers.readability().mobilize(new URL(entry.getLink()));
+                } catch (IOException ex) {
+                    description = entry.getDescription().getValue();
+                }
+            }
+
+            SyndContent content = new SyndContentImpl();
+            content.setType("text/html");
+            content.setValue(description);
+            entry.setDescription(content);
+        }
+    }
+
+    private String getExtendedDescription(SyndEntry entry) throws IOException {
+        assert entry != null;
+
+        Document html = Jsoup.connect(entry.getLink()).get();
+        Elements articleBodies = html.select("div[class=article__text]");
+
+        if(!articleBodies.isEmpty()) {
+            return articleBodies.get(0).html();
+        }
+
+        throw new IOException("No article text found on a given page: " + entry.getLink());
     }
 
     private static class MobileVersionLinkMapper implements LinkMapper {
